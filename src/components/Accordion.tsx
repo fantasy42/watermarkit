06912ad1fa36f462ac9e72630399836c70bacb7c
Fantasy <captainoftheship65@proton.me>
2025-12-16 09:26:10 +0100

    refactor: migrate from `@base-ui-components/react` to `@base-ui/react`

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,5 +1,5 @@
 'use client';
-import {Accordion} from '@base-ui-components/react/accordion';
+import {Accordion} from '@base-ui/react/accordion';
 import {Box, Flex, Text} from '@radix-ui/themes';
 
 import {PlusIcon} from '../icons/PlusIcon';
